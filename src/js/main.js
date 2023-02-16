cc2ad13abc2ea1c2b7d15a0656d06c6960b77b72
Ondřej Ševčík <Onas@Onas-macbook.local>
2023-02-16 21:18:28 +0100

    Responzivita price

--- src/js/main.js
+++ src/js/main.js
@@ -19,18 +19,14 @@ $(document) .ready(function(){
               breakpoint: 1200,
               settings: {
                 slidesToShow: 2,   
-            
               }
             },
             {
               breakpoint: 768,
               settings: {
-                slidesToShow: 2,
-                
-              }
-                                      
+                slidesToShow: 1,               
+              }                                     
             }]
-    });
-    
+    });   
 });
 
